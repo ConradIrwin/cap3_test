e23d6fb3cf0b8979d9eb5f9bdadbfec19e38e3f1
Conrad Irwin <conrad.irwin@gmail.com>
2014-03-14 16:17:10 -0700

    gitify

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cap3::Application.config.secret_key_base = '98882279425c964f532e45cb9def7615766a75381aac961d3054aea6681944ba7ce3593e639e462b446b93564ccc8be4bd98cfae99f0c7b6674699df923834cc'
